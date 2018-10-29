0101c2bb2f5bbe2ca057d8199f0ed50f9d86e84b
Аневризма <aneurysmjs@outlook.com>
2018-10-30 01:29:55 +0300

    feat (methods) add 'show' public method to 'Rectangle'

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod methods;
 
 fn main() {
   let rectangle = methods::rectangle::make(50, 35);
-  let area = rectangle.area();
-  println!("{:?}", area);
+  rectangle.show();
 }
--- src/methods/rectangle.rs
+++ src/methods/rectangle.rs
@@ -8,6 +8,10 @@ impl Rectangle {
   pub fn area(&self) -> u32 {
     self.height * self.width
   }
+
+  pub fn show(&self) {
+    println!("Area of {}X{}: {}", self.height, self.width, self.area());
+  }
   // related function
   fn new(height: u32, width: u32) -> Rectangle {
     Rectangle {
