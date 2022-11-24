0940ff92155c451e23334ab3e5b487a0afff956d
aneurysm <aneurysmjs@outlook.com>
2022-11-25 00:15:21 +0100

    feat: add regexes workspace

---
+++ regexes/src/basic.rs
@@ -0,0 +1,11 @@
+use regex::Regex;
+
+pub fn is_number(n: &str) -> bool {
+  let re = Regex::new(r"^\d").unwrap();
+
+  re.is_match(n)
+}
+
+#[cfg(test)]
+#[path = "./basic_test.rs"]
+mod basic_test;
---
+++ regexes/src/basic_test.rs
@@ -0,0 +1,8 @@
+use super::{ is_number };
+
+#[test]
+fn it_works() {
+  let result = is_number("2");
+
+  assert!(result)
+}
