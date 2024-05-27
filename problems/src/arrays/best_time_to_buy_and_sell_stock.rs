b6d7ec43fbe80f5224007a98b681808352281952
aneurysmjs <aneurysmjs@outlook.com>
2024-05-27 23:26:32 +0200

    feat(problems): add arrays

---
+++ problems/src/arrays/best_time_to_buy_and_sell_stock.rs
@@ -0,0 +1,18 @@
+pub fn calculate_profit(prices: &Vec<i32>) -> i32 {
+  let mut buy = prices[0];
+  let mut profit = 0;
+
+  for &price in prices.iter().skip(1) {
+    if price < buy {
+      buy = price;
+    } else if price - buy > profit {
+      profit = price - buy;
+    }
+  }
+
+  profit
+}
+
+#[cfg(test)]
+#[path = "./best_time_to_buy_and_sell_stock_test.rs"]
+mod best_time_to_buy_and_sell_stock_test;
---
+++ problems/src/lib.rs
@@ -0,0 +1,35 @@
+// src/lib.rs
+
+mod sum_vec;
+mod arrays;
+
+// pub fn rotate(some_vec: &mut Vec<i32>, k: i32) -> i32 {
+//   let mut total = 0;
+
+//   for i in 0..k {
+//     let last = some_vec.pop();
+//     println!("last {:?}", last);
+//     total += i;
+//   }
+
+//   total
+// }
+
+// pub fn max_profit(prices: &Vec<i32>) -> i32 {
+//   // if prices.is_empty() {
+//   //   0
+//   // }
+
+//   let mut buy = prices[0];
+//   let mut profit = 0;
+
+//   for &price in prices.iter().skip(1) {
+//     if price < buy {
+//       buy = price;
+//     } else if price - buy > profit {
+//       profit = price - buy;
+//     }
+//   }
+
+//   profit
+// }
