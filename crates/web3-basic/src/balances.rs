095f781a5e37f6a05d22d9720eec51e3f3a10158
aneurysmjs <aneurysmjs@outlook.com>
2025-10-08 01:17:12 +0200

    feat(web3-basic): add transfer

--- crates/web3-basic/src/balances.rs
+++ crates/web3-basic/src/balances.rs
@@ -9,12 +9,25 @@ impl Pallet {
         Self { balances: BTreeMap::new() }
     }
 
-    pub fn set_balance(&mut self, who: String, amount: u128) {
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
         self.balances.insert(who.clone(), amount);
     }
 
     pub fn get_balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
+    }
+
+    pub fn transfer(&mut self, from: &String, to: &String, amount: u128) -> Result<(), &str> {
+        let from_balance = self.get_balance(from);
+        let to_balance = self.get_balance(to);
+
+        let checked_from_balance = from_balance.checked_sub(amount).ok_or("Insufficient balance")?;
+        let checked_to_balance = to_balance.checked_add(amount).ok_or("Overflow when adding balance")?;
+
+        self.set_balance(from, checked_from_balance);
+        self.set_balance(to, checked_to_balance);
+
+        Ok(())
     }
 }
 
@@ -26,11 +39,61 @@ mod tests {
     fn test_balances() {
         let mut balances = Pallet::new();
 
-        balances.set_balance("Alice".into(), 100);
-        balances.set_balance("Bob".into(), 200);
+        balances.set_balance(&"Alice".to_string(), 100);
+        balances.set_balance(&"Bob".to_string(), 200);
 
-        assert_eq!(balances.get_balance(&"Alice".into()), 100);
-        assert_eq!(balances.get_balance(&"Bob".into()), 200);
-        assert_eq!(balances.get_balance(&"Charlie".into()), 0);
+        assert_eq!(balances.get_balance(&"Alice".to_string()), 100);
+        assert_eq!(balances.get_balance(&"Bob".to_string()), 200);
+        assert_eq!(balances.get_balance(&"Charlie".to_string()), 0);
+    }
+
+    #[test]
+    fn test_transfer() {
+        let alice = "Alice".to_string();
+        let bob = "Bob".to_string();
+
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&"Alice".to_string(), 100);
+        balances.set_balance(&"Bob".to_string(), 200);
+
+        let _ = balances.transfer(&alice, &bob, 50);
+
+        assert_eq!(balances.get_balance(&alice), 50);
+        assert_eq!(balances.get_balance(&bob), 250);
+    }
+
+    #[test]
+    fn test_transfer_insufficient_balance() {
+        let alice = "Alice".to_string();
+        let bob = "Bob".to_string();
+
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&"Alice".to_string(), 100);
+        balances.set_balance(&"Bob".to_string(), 200);
+
+        let result = balances.transfer(&alice, &bob, 500);
+
+        assert_eq!(result, Err("Insufficient balance"));
+        assert_eq!(balances.get_balance(&alice), 100);
+        assert_eq!(balances.get_balance(&bob), 200);
+    }
+
+    #[test]
+    fn test_transfer_overflow() {
+        let alice = "Alice".to_string();
+        let bob = "Bob".to_string();
+
+        let mut balances = Pallet::new();
+
+        balances.set_balance(&"Alice".to_string(), 100);
+        balances.set_balance(&"Bob".to_string(), u128::MAX);
+
+        let result = balances.transfer(&alice, &bob, 10);
+
+        assert_eq!(result, Err("Overflow when adding balance"));
+        assert_eq!(balances.get_balance(&alice), 100);
+        assert_eq!(balances.get_balance(&bob), u128::MAX);
     }
 }
--- crates/web3-basic/src/main.rs
+++ crates/web3-basic/src/main.rs
@@ -3,9 +3,11 @@ mod balances;
 fn main() {
     let mut balances = balances::Pallet::new();
 
-    balances.set_balance("Alice".into(), 100);
+    balances.set_balance(&"Alice".to_string(), 100);
 
-    let alice_balance = balances.get_balance(&"Alice".into());
+    let alice_balance = balances.get_balance(&"Alice".to_string());
+
+    balances.transfer(&"Alice".to_string(), &"Bob".to_string(), 50).unwrap();
 
     println!("Alice's balance is {}", alice_balance);
 }
